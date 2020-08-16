10de759c50fff962627e3e2b41f35a76343ef6c4
ANDREY BEZHENAR <69028191+andreybezhenar@users.noreply.github.com>
2020-08-16 00:58:49 -0700

    Update 8_fill_arrray_and_find_min_max_values.java

--- 8_fill_arrray_and_find_min_max_values.java
+++ 8_fill_arrray_and_find_min_max_values.java
@@ -1,19 +1,31 @@
 // problem: fill the array with random numbers and print the maximum, minimum and average values.
 // decision: 
 
-public class Test {
+import java.util.Arrays;
 
-public static void main(String[] args) {
-
-        int n = 60;
+public class MyTest {
+    public static void main(String args[]) {
+        
+        int n = 100;
+        double sum = 0;
         double[] array = new double[n];
         for (int i = 0; i < array.length; i++) {
             array[i] = Math.random();
+            sum+=array[i];
         }
-
-        double max = array[0]; // array doesn't have to be empty.
+        
+        // version 1: using imprort java.util.Arrays.
+        Arrays.sort(array);
+        System.out.println("My idea for max: " + array[n - 1]);
+        System.out.println("My idea for min: " + array[0]);
+        System.out.println("My idea for avg: " + sum/array.length);
+        System.out.println();
+        
+        // version 2: using conditional statements:
+        double max = array[0]; 
         double min = array[0];
         double avg = 0;
+        
         for (int i = 0; i < array.length; i++) {
             if(max < array[i])
                 max = array[i];
@@ -21,9 +33,10 @@ public static void main(String[] args) {
                 min = array[i];
             avg += array[i]/array.length;
         }
-
+        
         System.out.println("max = " + max);
         System.out.println("min = " + min);
         System.out.println("avg = " + avg);
+        
     }
 }
