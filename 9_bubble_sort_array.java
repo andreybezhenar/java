229e9ded2bc39c4fb77840934484a996a9a5d42c
ANDREY BEZHENAR <69028191+andreybezhenar@users.noreply.github.com>
2020-08-21 23:48:29 -0700

    Create 9_bubble_sort_array.java

---
+++ 9_bubble_sort_array.java
@@ -0,0 +1,16 @@
+// problem: implement a bubble sort algorithm to sort an array.
+// decision: 
+        
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
