a032077554cc7cd1966750caf0843d5fe50866e7
ANDREY BEZHENAR <69028191+andreybezhenar@users.noreply.github.com>
2020-08-22 18:36:02 -0700

    Update 9_bubble_sort_array.java

--- 9_bubble_sort_array.java
+++ 9_bubble_sort_array.java
@@ -1,16 +1,35 @@
 // problem: implement a bubble sort algorithm to sort an array.
 // decision: 
+
+import java.util.Arrays;
+
+public class BubleSort {
+    
+    public static void main(String[] args) {
         
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    double temp = array[j];
+        int[] array = {8, 2, 4, 6};
+        
+        System.out.println("Unsorted: " + Arrays.toString(array));
+        
+        // we make this counter to limit number of checks.
+        for (int i = 0; i < array.length; i++) { 
+            
+            
+            // we make this counter to compare two elements of array.
+            for (int j = 0; j < array.length - i - 1; j++) { 
+                
+                
+                if (array[j] > array[j + 1]){
+                    
+                    // we create buffer variable to swap the values.
+                    int buf = array[j];
                     array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    array[j + 1] = buf;
+                    
                 }
             }
         }
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+        
+        System.out.println("Sorted: " + Arrays.toString(array));
+    }
+}
